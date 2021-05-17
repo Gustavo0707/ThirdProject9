1d540f327cd32d2e53c707f61246f63aa937291f
Gustavo0707 <gustavomelo0703@gmail.com>
2021-05-17 14:41:01 -0300

    Initial commit

---
+++ src/Gustavotestescom/company/Main.java
@@ -0,0 +1,15 @@
+package Gustavotestescom.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int num = 1; num <= 50; num++) {
+            if ((num%2) == 1) {
+                System.out.println(num);
+            }
+        }
+
+    }
+}
